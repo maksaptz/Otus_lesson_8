278aea9a6f5cc8a85427aaf2f7af7a1b542fe7e2
maksaptz <128064182+maksaptz@users.noreply.github.com>
2023-06-05 19:08:30 +0300

    Update httpd.sh

--- httpd.sh
+++ httpd.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-yum install httpd -y
+yum install httpd -y ### устанавливаем апач
 
-sed '22a\EnvironmentFile=/etc/sysconfig/httpd-%I' /usr/lib/systemd/system/httpd.service
-touch /etc/sysconfig/{httpd-first,httpd-second}
-echo 'OPTIONS=-f conf/first.conf' > /etc/sysconfig/httpd-first
-echo 'OPTIONS=-f conf/second.conf' > /etc/sysconfig/httpd-second
-cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/first.conf
-cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/second.conf
-sed -i 's/Listen 80/Listen 8080/' /etc/httpd/conf/second.conf
-echo 'PidFile /var/run/httpd-second.pid' >> /etc/httpd/conf/second.conf
-systemctl start httpd@first
+sed '22a\EnvironmentFile=/etc/sysconfig/httpd-%I' /usr/lib/systemd/system/httpd.service ### правим юнит systemd, чтобы сделать доступным шаблоны
+touch /etc/sysconfig/{httpd-first,httpd-second} ### создаем файлы которые будут отсылать систему к конфигам
+echo 'OPTIONS=-f conf/first.conf' > /etc/sysconfig/httpd-first ### указываем путь к 1 конфигу
+echo 'OPTIONS=-f conf/second.conf' > /etc/sysconfig/httpd-second ### указываем путь ко 2 конфигу
+cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/first.conf ### создаем 1 конфиг
+cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/second.conf ### создаем 2 конфин
+sed -i 's/Listen 80/Listen 8080/' /etc/httpd/conf/second.conf ### правим порт во твором конфигу
+echo 'PidFile /var/run/httpd-second.pid' >> /etc/httpd/conf/second.conf ### правим № процесса
+systemctl start httpd@first ### запускаем апач
 systemctl start httpd@second
