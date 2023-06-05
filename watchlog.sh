cbfeedbe85286889dcbd5ab765e941622e096969
maksaptz <128064182+maksaptz@users.noreply.github.com>
2023-06-05 19:13:41 +0300

    Update watchlog.sh

--- watchlog.sh
+++ watchlog.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cat << EOF > /etc/sysconfig/watchlog
+cat << EOF > /etc/sysconfig/watchlog ### создаем конфиг
 # Configuration file for my watchlog service
 # Place it to /etc/sysconfig
 # File and word in that file that we will be monit
@@ -8,7 +8,7 @@ WORD="ALERT"
 LOG=/var/log/watchlog.log
 EOF
 
-cat << EOF > /var/log/watchlog.log
+cat << EOF > /var/log/watchlog.log ### создаем тестовый лог
 dsnjkadhsjadbsadsa
 dsgfh ALERT
 dasdsabgfdgh
@@ -18,7 +18,7 @@ dsadsadsadsadsa
 dsadsdsadsdsasdadsadaasd
 EOF
 
-cat << EOF > /opt/watchlog.sh
+cat << EOF > /opt/watchlog.sh  ### пишем скрипт поиска ключевого слова
 #!/bin/bash
 
 WORD=\$1
@@ -33,9 +33,9 @@ exit 0
 fi
 EOF
 
-chmod +x /opt/watchlog.sh
+chmod +x /opt/watchlog.sh ### даем права скрипту на исполнение
 
-cat << EOF > /etc/systemd/system/watchlog.service
+cat << EOF > /etc/systemd/system/watchlog.service  ### создаем юнит
 
 [Unit]
 Description=My watchlog service
@@ -46,7 +46,7 @@ EnvironmentFile=/etc/sysconfig/watchlog
 ExecStart=/opt/watchlog.sh \$WORD \$LOG
 EOF
 
-cat << EOF > /etc/systemd/system/watchlog.timer
+cat << EOF > /etc/systemd/system/watchlog.timer ### создаем timer
 
 [Unit]
 Description=Run watchlog script every 30 second
@@ -60,6 +60,6 @@ Unit=watchlog.service
 WantedBy=multi-user.target
 EOF
 
-systemctl daemon-reload
-systemctl start watchlog.timer
-systemctl start watchlog.service
+systemctl daemon-reload ### перечитываем юниты
+systemctl start watchlog.timer ### стартуем таймер
+systemctl start watchlog.service ### стартуем сам юнит
